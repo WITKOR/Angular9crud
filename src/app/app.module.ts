ae7a7026a61bd7d21a53bbe88a9e8f10c98b6799
WITKOR <vitalii.korneliuk99@gmail.com>
2020-07-21 23:59:49 +0200

    adding main code

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import {FormsModule} from '@angular/forms'
+
+import {AngularFireModule} from '@angular/fire';
+import {AngularFireDatabaseModule} from '@angular/fire/database';
+import {environment} from '../environments/environment';
+import {AngularFirestoreModule} from '@angular/fire/firestore';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { from } from 'rxjs';
+
+import {CrudService} from './service/crud.service';
+ 
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    AngularFirestoreModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBbdVoMldXQSfDnYjPduC6-2C3AyxYSmtU",
+    authDomain: "ng-firebase-6befe.firebaseapp.com",
+    databaseURL: "https://ng-firebase-6befe.firebaseio.com",
+    projectId: "ng-firebase-6befe",
+    storageBucket: "ng-firebase-6befe.appspot.com",
+    messagingSenderId: "840117178662",
+    appId: "1:840117178662:web:f373cc55236e53612731e7",
+    measurementId: "G-L84SFWKFV7"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
